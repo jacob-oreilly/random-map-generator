a682b13ebdcc63131afded13211026e1aa73999d
jacob-oreilly <jakeor45@gmail.com>
2021-09-29 23:55:31 -0600

    Set up basic sdl2 window. Slightly fixed perlin noise but is outputing out of bounds a little bit? Next step is to fix this issue and then figure out how to print gray scale map with sdl

--- main.cpp
+++ main.cpp
@@ -1,82 +1,105 @@
 #include <iostream>
 #include <cmath>
 #include "perlinnoise.h"
+#include <SDL2/SDL.h>
+#include <stdio.h>
 using namespace std;
 
-//int repeat = -1;
-//static setRepeat(int rep)  {
-//    repeat = rep;
-//}
-//double OctavePerlin(double x, double y, double z, int octaves, double persistence) {
-//    double total = 0;
-//    double frequency = 1;
-//    double amplitude = 1;
-//    double maxValue = 0;			// Used for normalizing result to 0.0 - 1.0
-//    for(int i=0;i<octaves;i++) {
-//        total += perlin(x * frequency, y * frequency, z * frequency) * amplitude;
-//
-//        maxValue += amplitude;
-//
-//        amplitude *= persistence;
-//        frequency *= 2;
-//    }
-//
-//    return total/maxValue;
-//}
-//
-//float GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
-//    float noiseMap[mapHeight, mapWidth];
-//
-//    if(scale <= 0) {
-//        scale = 0.001;
-//    }
-//    for(int i = 0; i < mapHeight; i++) {
-//        for(int j = 0; j < mapWidth; j++) {
-//            float sampleX = i / scale;
-//            float sampleY = j / scale;
-//            //float perlinValue
-//        }
-//    }
-//    return 0.0;
-//}
+const int screenWidth = 600;
+const int screenHeight = 600;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* window = NULL;
+SDL_Surface* screenSurface = NULL;
+SDL_Surface* mapSurface = NULL;
+
+bool init() {
+
+    bool success = true;
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+     else
+    {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+        }
+    }
+
+    return success;
+
+}
+
+bool loadMedia() {
+    bool success = true;
+    //build map from array
+    return success;
+}
+
+void close() {
+    SDL_FreeSurface(mapSurface);
+    mapSurface = NULL;
+    //Destroy window
+    SDL_DestroyWindow( window );
+    window = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+}
 // main() is where program execution begins.
-int main()
+int main(int argc, char* args[])
 {
-    unsigned int seed = 237;
+    srand(time(NULL));
+    int seed = rand() % 256;
     PerlinNoise pn(seed);
     unsigned int kk = 0;
-    int screenWidth = 10;
-    int screenHeight = 10;
-//    int maxNumberBlockHeight = screenWidth / 32;
-//    int maxNumberBlockWidth = screenHeight / 32;
+    int maxNumberBlockHeight = screenWidth / 8;
+    int maxNumberBlockWidth = screenHeight / 8;
 
-    int a[screenHeight][screenWidth];
-    for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
+    int a[screenHeight][screenHeight];
+    for (int i = 0; i < screenHeight; i++)
     {
         //[i][0]=rand()%2;
-        for (int j = 0; j < sizeof(a[i]) / sizeof(a[i][0]); j++)
+        for (int j = 0; j < screenWidth; j++)
         {
-            //Old random generator
-            //int oneProbability = rand()%100;
-            //cout<<"2D array have: "<< a[i][j]<<endl;
-            //out = out + std::to_string(a[i][j]);
-            //a[i][j] = rand()%2;
-            // << "rand Numb: " << oneProbability;
-            //if (oneProbability >= 30 && oneProbability <= 90) {
-            //    a[i][j] = 1;
-            //}
-            //else {
-             //   a[i][j] = 0;
-            //}
             double x  = (double)j/((double)screenWidth);
             double y = (double)i/((double)screenHeight);
-            double n = pn.noise(10 * x, 10 * y, 0.3);
-            a[i][j] = floor(255 * n);
+            double n = pn.noise(10 * x, 10 * y, 0.4);
+            a[i][j] = n;//floor(255 * n);
             cout << a[i][j] << " \n"[j == sizeof(a[i]) / sizeof(a[i][0])-1];
         }
-        //cout<<"Map: "<< out<<endl;
     }
-    cout << "Hello World"; // prints Hello World
+
+    if(!init()) {
+        printf("Failed to Initialize!\n");
+    }
+    else {
+        if(!loadMedia()) {
+            printf("Failed to load media!\n");
+        }
+        else {
+            //Draw map
+            //Update surface
+            SDL_Delay(2000);
+        }
+    }
+
+    close();
     return 0;
 }
 
